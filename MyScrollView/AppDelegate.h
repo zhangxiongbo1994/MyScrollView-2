988a31234219221df650ba43f0dd8716003ba937
刘刘智明 <liuliuzhiming@liuliuzhimingdemacbook-air.local>
2016-06-28 14:05:47 +0800

    Initial Commit

---
+++ MyScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyScrollView
+//
+//  Created by 刘刘智明 on 16/6/28.
+//  Copyright © 2016年 lzm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyScrollView
+//
+//  Created by 刘刘智明 on 16/6/28.
+//  Copyright © 2016年 lzm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
